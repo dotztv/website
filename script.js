5031c28c987fadc931cc011b047e0b8e392f07e0
Simplicity <68008162+dotzSimplicity@users.noreply.github.com>
2023-08-30 16:54:34 +0200

    count

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let count = 0;
 
 const increment = () => {
   count++;
-  console.log("This website has been loaded on your browser for = " + count);
+  console.log("This website has been loaded on your browser for = " + count + " Seconds!");
 };
 
 setInterval(increment, 1000);
