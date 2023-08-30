a2d983d3c4ebadf39c4a78bd09b68141bee1b798
Simplicity <68008162+dotzSimplicity@users.noreply.github.com>
2023-08-30 16:52:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log("Hello World!")
+
+let count = 0;
+
+const increment = () => {
+  count++;
+  console.log("This website has been loaded on your browser for = " + count);
+};
+
+setInterval(increment, 1000);
